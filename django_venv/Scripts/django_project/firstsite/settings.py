0b23878171f8d1bf3241750ce4d4aaabadc9c15d
YouSang Kim <sang981113@naver.com>
2022-12-08 14:52:38 +0900

    포스트 목록 페이지에 대표 이미지 출력하기

--- django_venv/Scripts/django_project/firstsite/settings.py
+++ django_venv/Scripts/django_project/firstsite/settings.py
@@ -118,8 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MDEIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
