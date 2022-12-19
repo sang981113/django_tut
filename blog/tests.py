4d032bd93bdf67f6937ecfd84700faeabdd8343e
YouSang Kim <sang981113@naver.com>
2022-12-19 10:06:42 +0900

    포스트 목록, 상세 페이지에서 작성자 정보 출력하기

--- blog/tests.py
+++ blog/tests.py
@@ -1,10 +1,13 @@
 from django.test import TestCase, Client
 from bs4 import BeautifulSoup
 from .models import Post
+from django.contrib.auth.models import User
 
 class TestView(TestCase):
     def setUp(self):
         self.client = Client()
+        self.user_trump = User.objects.create_user(username='trump', password='somepassword')
+        self.user_obama = User.objects.create_user(username='obama', password='somepassword')
 
     
     def navbar_test(self, soup):
@@ -25,51 +28,44 @@ class TestView(TestCase):
         self.assertEqual(about_me_btn.attrs['href'], '/about_me/')
 
     def test_post_list(self):
-        # 1.1 포스트 목록 페이지를 가져온다.
         response = self.client.get('/blog/')
-        # 1.2 정상적으로 페이지가 로드된다.
         self.assertEqual(response.status_code, 200)
-        # 1.3 페이지 타이틀은 'Blog'이다.
         soup = BeautifulSoup(response.content, 'html.parser')
         self.assertEqual(soup.title.text, 'Blog')
-        # 1.4 내비게이션 바가 있다.
-        # 1.5 Blog, About Me라는 문구가 내비게이션 바에 있다.
         self.navbar_test(soup)
 
-        # 2.1 메인 영역에 게시물이 하나도 없다면
         self.assertEqual(Post.objects.count(), 0)
-        # 2.2 '아직 게시물이 없습니다'라는 문구가 보인다.
         main_area = soup.find('div', id='main-area')
         self.assertIn('아직 게시물이 없습니다', main_area.text)
 
-        # 3.1 게시물이 2개 있다면
         post_001 = Post.objects.create(
             title='첫 번째 포스트입니다.',
             content='Hello World. We are the world.',
+            author=self.user_trump
         )
         post_002 = Post.objects.create(
             title='두 번째 포스트입니다.',
             content='1등이 전부는 아니잖아요?',
+            author=self.user_obama
         )
         self.assertEqual(Post.objects.count(), 2)
-        # 3.2 포스트 목록 페이지를 새로고침했을 때,
         response = self.client.get('/blog/')
         soup = BeautifulSoup(response.content, 'html.parser')
         self.assertEqual(response.status_code, 200)
-        # 3.3 메인 영역에 포스트 2개의 타이틀이 존재한다.
         main_area = soup.find('div', id='main-area')
         self.assertIn(post_001.title, main_area.text)
         self.assertIn(post_002.title, main_area.text)
-        # 3.4 '아직 게시물이 없습니다'라는 문구는 더 이상 보이지 않는다.s
         self.assertNotIn('아직 게시물이 없습니다', main_area.text)
 
+        self.assertIn(self.user_trump.username.upper(), main_area.text)
+        self.assertIn(self.user_obama.username.upper(), main_area.text)
+
     def test_post_detail(self):
-        # 1.1 포스트가 하나 있다.
         post_001 = Post.objects.create(
             title='첫 번째 포스트입니다.',
             content='Hello World. We are the world.',
+            author=self.user_trump
         )
-        # 1.2 그 포스트의 url은 '/blog/1/' 이다.
         self.assertEqual(post_001.get_absolute_url(), '/blog/1/')
 
         # 2. 첫 번째 포스트의 상세 페이지 테스트
@@ -85,7 +81,9 @@ class TestView(TestCase):
         main_area = soup.find('div', id='main-area')
         post_area = main_area.find('div', id='post-area')
         self.assertIn(post_001.title, post_area.text)
-        # 2.5 첫 번재 포스트의 작성자가 포스트 영역에 있다.(아직 구현안함)
-
+        # 2.5 첫 번재 포스트의 작성자가 포스트 영역에 있다.
+        self.assertIn(self.user_trump.username.upper(), post_area.text)
         # 2.6 첫 번재 포스트의 내용이 포스트 영역에 있다.
         self.assertIn(post_001.content, post_area.text)
+
+    
